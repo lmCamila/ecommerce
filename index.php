3ff0f90071b9921235dfec3586106693f3e78711
lmCamila <camilalm31@gmail.com>
2018-07-31 08:05:37 -0300

    Site - categorias

--- index.php
+++ index.php
@@ -273,7 +273,7 @@ $app->get('/admin/categories/:idcategory', function($idcategory){
 
 $app->post('/admin/categories/:idcategory', function($idcategory){
 	User::verifyLogin();
-	
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
@@ -287,7 +287,18 @@ $app->post('/admin/categories/:idcategory', function($idcategory){
 	exit();
 });
 
+$app->get('/categories/:idcategory',function($idcategory){
+	$category = new Category();
 
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category",[
+		'category'=>$category->getValues(),
+		'products'=>[]
+	]);
+});
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -25,6 +25,8 @@ class Category extends Model{
 		));
 
 		$this->setData($results[0]);
+
+		Category::updateFile();
 	}
 
 	public function get($idcategory)
@@ -36,7 +38,7 @@ class Category extends Model{
 		));
 		$this->setData($results[0]);
 	}
-	
+
 	public function delete()
 	{
 		$sql = new Sql();
@@ -44,6 +46,20 @@ class Category extends Model{
 		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory",array(
 			":idcategory"=>$this->getidcategory()
 		));
+
+		Category::updateFile();
+	}
+
+	public static function updateFile(){
+		$categories = Category::listAll();
+
+		$html = [];
+
+		foreach($categories as $row){
+			array_push($html , '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+		}
+
+		file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."categories-menu.html",implode('',$html));
 	}
 }
 
