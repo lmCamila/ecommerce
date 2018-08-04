8ebb2dbf1ae2ec8d24aa617e9086932c45147fed
lmCamila <camilalm31@gmail.com>
2018-08-04 08:21:53 -0300

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -50,7 +50,8 @@ class Category extends Model{
 		Category::updateFile();
 	}
 
-	public static function updateFile(){
+	public static function updateFile()
+	{
 		$categories = Category::listAll();
 
 		$html = [];
@@ -84,6 +85,33 @@ class Category extends Model{
 				]);
 		}
 	}
+
+	public function getProductsPage($page = 1, $itemsPerPage = 12)
+	{
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products a
+			INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+			INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+			WHERE c.idcategory = :idcategory
+			LIMIT $start, $itemsPerPage;
+			",[
+				':idcategory'=>$this->getidcategory()
+			]);
+
+		$resultsTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>Product::checkList($results),
+			'total'=>(int)$resultsTotal[0]["nrtotal"],
+			'pages'=>ceil($resultsTotal[0]["nrtotal"] / $itemsPerPage)
+		];
+	}
+
 	public function addProduct(Product $product)
 	{
 		$sql = new Sql();
