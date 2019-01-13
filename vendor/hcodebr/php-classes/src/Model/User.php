acf4a33958f71409f8c58f8df192ad6cd682a86b
lmCamila <camilalm31@gmail.com>
2019-01-13 04:19:54 -0200

    Minha Conta - Alterar Dados

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -12,6 +12,7 @@ class User extends Model{
 	const SECRET = "l8qibxOhTABtyl6nywXMGYHkjxgjwo5mk9cfBVTs0eY=";
 	const ERROR = "UserError";
 	const ERROR_REGISTER = "UserErrorRegister";
+	const SUCCESS = "Success";
 	
 	public static function getFromSession()
 	{
@@ -310,6 +311,24 @@ class User extends Model{
 		return (count($results) > 0);
 	}
 
+	public static function setSuccess($msg)
+	{
+		$_SESSION[User::SUCCESS] = $msg;
+	}
+
+	public static function getSuccess()
+	{
+		$msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS] )? $_SESSION[User::SUCCESS] : '';
+
+		User::clearErrorRegister();
+
+		return $msg;
+	}
+
+	public static function clearSuccess()
+	{
+		$_SESSION[User::SUCCESS] = NULL;
+	}
 }
 
  ?>
