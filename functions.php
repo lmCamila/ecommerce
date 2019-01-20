87916e70c4cb83c4729deea7df8324d99fc7394d
lmCamila <camilalm31@gmail.com>
2019-01-20 05:37:47 -0200

    Meus Pedidos e Resumo Carrinho

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php 
 
 use \Hcode\Model\User;
+use \Hcode\Model\Cart;
 
 function formatPrice( $nps)
 {
@@ -25,4 +26,22 @@ function getUserName()
 
 }
 
+function getCartNrQtd()
+{
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return $totals['nrqtd'];
+}
+
+
+function getCartVlSubTotal()
+{
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return formatPrice($totals['vlprice']);
+}
  ?>
